c15f69779abbc46d5b137bf8c23e79dbea3dbcbf
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-06-25 14:43:33 +0400

    return jsx and MemeGenerator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import './App.css';
+import MemeGenerator from './MemeGenerator';
+
 const App = () => {
-    return <h1>Hello, World!</h1>;
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="content-box">
+                    <img
+                        className="logo"
+                        src="/images/meme-gpt-logo.png"
+                        alt="Meme GPT logo"
+                    />
+                    <p>
+                        Click the button below to generate a meme based on
+                        what's trending.
+                    </p>
+                </div>
+                <MemeGenerator />
+            </div>
+        </div>
+    );
 };
 
 export default App;
