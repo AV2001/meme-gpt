cb0e8449ec5ad1cd12ace101d4229cbeb8a19d61
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-06-25 14:44:11 +0400

    render meme and the image

--- src/MemeGenerator.jsx
+++ src/MemeGenerator.jsx
@@ -9,7 +9,7 @@ const MemeGenerator = () => {
         try {
             setLoading(true);
             const response = await fetch(
-                `${process.env.REACT_APP_BACKEND_URL}/openai`
+                `${import.meta.env.VITE_BACKEND_URL}/openai`
             );
             const body = await response.json();
             const meme = body.content;
@@ -17,9 +17,45 @@ const MemeGenerator = () => {
             setLoading(false);
             setError('');
         } catch (error) {
+            setLoading(false);
             setError('Oops!');
         }
     };
+
+    const renderResponse = () => {
+        const randomNum = Math.trunc(Math.random() * 17) + 1;
+        return (
+            <div className="meme-container">
+                {loading ? (
+                    <div className="spinner"></div>
+                ) : (
+                    result && (
+                        <>
+                            <p className="meme-text">
+                                {result.toUpperCase().replace(/["]/g, '')}
+                            </p>
+                            <img
+                                className="meme-img"
+                                src={`/images/elon_vs_mark(${randomNum}).jpg`}
+                                alt="Elon Musk and Mark Zuckerberg"
+                            />
+                        </>
+                    )
+                )}
+            </div>
+        );
+    };
+
+    console.log(result);
+
+    return (
+        <>
+            <button className="btn" onClick={getData}>
+                {loading ? 'Generating...' : 'Generate Meme'}
+            </button>
+            {renderResponse()}
+        </>
+    );
 };
 
 export default MemeGenerator;
