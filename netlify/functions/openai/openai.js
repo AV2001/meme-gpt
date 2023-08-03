1d23cd1a8ce21ed7739749d9e9edf1e4f81c840c
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-08-03 15:28:11 +0400

    accept api key from the frontend to send a request to openai

--- netlify/functions/openai/openai.js
+++ netlify/functions/openai/openai.js
@@ -1,52 +1,55 @@
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
 const handler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'system',
+    // Parse the request body to get the JSON object
+    const requestBody = JSON.parse(event.body);
 
-          // ELON MUSK VS MARK ZUCKERBERG FIGHT
-          content: `You are an extremely funny and are well-versed with memes. I will be asking you to provide a text for the meme in the following context. The context is that Elon Musk has implemented rate limits on Twitter and let's see what Mark Zuckerberg has to say to this. Make sure that you reply within 70 characters.`,
-        },
-        {
-          role: 'user',
-          content:
-            'Generate simple text for a meme in the context I provided and make sure that you break the internet.',
-        },
-      ],
+    const apiKey = requestBody.apiKey;
+
+    const configuration = new Configuration({
+        apiKey: apiKey,
     });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data.choices[0].message.content),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to connect to OpenAI' }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-  }
+    const openai = new OpenAIApi(configuration);
+
+    try {
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'system',
+                    content: `You are an extremely funny and are well-versed with memes. I will be asking you to provide a text for the meme in the following context. The context is that Elon Musk has implemented rate limits on Twitter and let's see what Mark Zuckerberg has to say to this. Make sure that you reply within 70 characters.`,
+                },
+                {
+                    role: 'user',
+                    content:
+                        'Generate simple text for a meme in the context I provided and make sure that you break the internet.',
+                },
+            ],
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data.choices[0].message.content),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to connect to OpenAI' }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+    }
 };
 
 exports.handler = handler;
