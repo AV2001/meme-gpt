ff76a424604db22e0a2f5ca861b1149ba71b8cba
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-08-03 15:27:40 +0400

    allow user to add openai api key

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import './App.css';
 import MemeGenerator from './MemeGenerator';
+import { useState } from 'react';
 
 const App = () => {
+    const [apiKey, setApiKey] = useState('');
+
     return (
-        <div className="App">
-            <div className="container">
-                <div className="content-box">
+        <div className='App'>
+            <div className='container'>
+                <div className='content-box'>
                     <img
-                        className="logo"
-                        src="/images/meme-gpt-logo.png"
-                        alt="Meme GPT logo"
+                        className='logo'
+                        src='/images/meme-gpt-logo.png'
+                        alt='Meme GPT logo'
                     />
                     <p>
                         Click the button below to generate a meme based on
                         what's trending.
                     </p>
+                    <input
+                        type='text'
+                        placeholder='Enter your OpenAI API Key'
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                    />
                 </div>
-                <MemeGenerator />
+                <MemeGenerator apiKey={apiKey} />
             </div>
         </div>
     );
